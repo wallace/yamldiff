9752080638058c1ff5ece8aa37fa42fad522931c
Daniël de Vries <danielpunt@gmail.com>
2014-10-08 19:52:06 +0200

    added #path method to reduce duplication in error classes

--- lib/yamldiff/yamldiff_error.rb
+++ lib/yamldiff/yamldiff_error.rb
@@ -1,35 +1,40 @@
 class YamldiffError
   attr_reader :key, :context
+
   def initialize(key, context)
     @key = key
     @context = context
+  end
+
+  def path
+    (@context + [@key]).join('.')
   end
 end
 
 class YamldiffKeyError < YamldiffError
   def to_s
-    "Missing key: #{@context.join(".")}.#{@key}"
+    "Missing key: #{path}"
   end
 end
 
 class YamldiffKeyValueTypeError < YamldiffError
   def to_s
-    "Key value type mismatch: #{@context.join(".")}.#{@key}"
+    "Key value type mismatch: #{path}"
   end
 end
 
 class YamldiffKeyValueError < YamldiffError
+  attr_reader :diff
+
   def initialize(key, context, diff = nil)
     super key, context
     @diff = diff
   end
 
   def to_s
-    output = []
-    output << "Key content differs: #{@context.join(".")}.#{@key}"
+    output = ["Key content differs: #{path}"]
     if @diff
-      output << "Diff:"
-      output << @diff
+      output << "Diff:" << @diff
     end
     output.join("\n")
   end
